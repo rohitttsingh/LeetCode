f8da45bce8de2abb78f38f076317e8964b75d4ef
Rohit Singh <73903627+rohitttsingh@users.noreply.github.com>
2022-12-14 18:17:25 +0530

    Create 5. Longest Palindromic Substring.cpp

---
+++ 5. Longest Palindromic Substring.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+        string a = "";
+
+   void checkPalindrome(string str){
+       string cpy =  str;
+       reverse(str.begin(), str.end());
+
+       if(cpy==str){
+           if(a.length()<cpy.length())
+            a = cpy;
+       }
+   }
+
+    string longestPalindrome(string s) {
+        int n = s.length();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            checkPalindrome(s.substr(i,j));//<<" ";
+       }
+     }
+    
+    return a;
+}
+};
